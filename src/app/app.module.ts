57325c57a5033e36084d155918188f0278b36f3e
Estevão Rodrigues <estevao.rodrigues@optsol.com.br>
2022-11-28 23:23:46 -0300

    Melhorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { CardComponent } from './card/card.component';
+import { ExperienciasComponent } from './experiencias/experiencias.component';
+import { ParceirosComponent } from './parceiros/parceiros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ServicosComponent,
+    CardComponent,
+    ExperienciasComponent,
+    ParceirosComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/parceiros/parceiros.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parceiros',
+  templateUrl: './parceiros.component.html',
+  styles: [
+  ]
+})
+export class ParceirosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
